c9a99c4c604d1b4e41c9a83fa8ce9f5fa24e9f2d
Raphael Ramirez <ramirez.raph@yahoo.com>
2023-03-28 12:23:29 +0800

    Project Setup

---
+++ JobBoards.Api/Program.cs
@@ -0,0 +1,21 @@
+using JobBoards.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddData();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
+
+var app = builder.Build();
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
+}
+
---
+++ JobBoards.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JobBoards.WebApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

221c17a7b397e474a539b3477516a0d724f0562a
Raphael Ramirez <ramirez.raph@yahoo.com>
2023-03-29 17:27:54 +0800

    updated npm versions

--- JobBoards.WebApplication/tailwind.config.js
+++ JobBoards.WebApplication/tailwind.config.js
@@ -1,20 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        '!**/{bin,obj,node_modules}/**',
-        '**/*.{cshtml,html}'
-    ],
-    prefix: 'tw-',
-    corePlugins: {
-        preflight: false,
-    },
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  content: [
+    '!**/{bin,obj,node_modules}/**',
+    '**/*.{cshtml,html}'
+  ],
+  prefix: 'tw-',
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
 

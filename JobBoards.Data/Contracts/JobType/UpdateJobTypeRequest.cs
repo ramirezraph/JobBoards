f04874a2ada10f255714af286b13f9d76b0007c3
joweynhan <cs.joagneannfernando@gmail.com>
2023-03-30 16:11:13 +0800

    JobTypeAPI

---
+++ JobBoards.Data/Contracts/JobType/UpdateJobTypeRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace JobBoards.Data.Contracts.JobType
+{
+    public class UpdateJobTypeRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public UpdateJobTypeRequest(string name, string? description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}

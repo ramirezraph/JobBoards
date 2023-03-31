c9fe6e838dc086c732df0b82e3eabfa902c20d43
Raphael Ramirez <ramirez.raph@yahoo.com>
2023-03-31 21:20:05 +0800

    setup controllers and views

--- JobBoards.WebApplication/Controllers/JobsController.cs
+++ JobBoards.WebApplication/Controllers/JobsController.cs
@@ -4,6 +4,9 @@ namespace JobBoards.WebApplication.Controllers;
 
 public class JobsController : Controller
 {
+
+    // ANONYMOUS
+
     [HttpGet]
     public IActionResult Index()
     {
@@ -16,14 +19,28 @@ public class JobsController : Controller
         return View();
     }
 
+    // EMPLOYER
+
     [HttpGet]
-    public IActionResult Manage()
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Edit()
     {
         return View();
     }
 
     [HttpGet]
     public IActionResult ManageDetails()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult ManageJobApplications()
     {
         return View();
     }

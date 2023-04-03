abe0bc4340fa4b9ea639c988fc3451f7ae4a495a
joagneann <cs.joagneannfernando@gmail.com>
2023-04-03 22:32:12 +0800

    JobPostAPI

---
+++ JobBoards.Api/Controllers/JobPostsController.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using JobBoards.Data.Contracts.JobLocation;
+using JobBoards.Data.Contracts.JobPost;
+using JobBoards.Data.Entities;
+using JobBoards.Data.Persistence.Repositories.JobLocations;
+using JobBoards.Data.Persistence.Repositories.JobPosts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobBoards.Api.Controllers;
+
+    public class JobPostsController : ApiController
+    {
+        private readonly IJobPostsRepository _jobPostsRepository;
+        private readonly IMapper _mapper;
+
+        public JobPostsController(IJobPostsRepository jobPostsRepository, IMapper mapper)
+        {
+            _jobPostsRepository = jobPostsRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListJobPosts()
+        {
+            List<JobPost> jobPosts = await _jobPostsRepository.GetAllAsync();
+            return Ok(jobPosts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetJobPostById(Guid id)
+        {
+            var jobPost = await _jobPostsRepository.GetByIdAsync(id);
+            if (jobPost is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(jobPost);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateJobPost(CreateJobPostRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newjobPost = JobPost.CreateNew(request.Title, request.Description, request.JobLocationId, request.MinSalary, request.MaxSalary, request.IsActive, request.JobCategoryId, request.JobTypeId, request.Expiration, request.CreatedById);
+            await _jobPostsRepository.AddAsync(newjobPost);
+
+            return CreatedAtAction(nameof(GetJobPostById), new { id = newjobPost.Id }, newjobPost);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJobLocation(Guid id)
+        {
+            var jobPost = await _jobPostsRepository.GetByIdAsync(id);
+            if (jobPost is null)
+            {
+                return NotFound();
+            }
+
+            await _jobPostsRepository.RemoveAsync(jobPost);
+
+            return NoContent();
+        }
+
+    [HttpPost("{id}")]
+    public async Task<IActionResult> UpdateJobPost(UpdateJobPostRequest request, Guid id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var jobPost = await _jobPostsRepository.GetByIdAsync(id);
+        if (jobPost is null)
+        {
+            return NotFound();
+        }
+
+        var updatedJobPost = _mapper.Map<JobPost>(request);
+
+        await _jobPostsRepository.UpdateAsync(jobPost.Id, updatedJobPost);
+
+        return Ok();
+    }
+}
+
---
+++ JobBoards.Data/Contracts/JobPost/UpdateJobPostRequest.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobBoards.Data.Contracts.JobPost;
+
+public class UpdateJobPostRequest
+{
+    [Required]
+    public string Title { get; set; } = default!;
+
+    [Required]
+    public string Description { get; set; } = default!;
+
+    [Required]
+    public Guid JobLocationId { get; set; }
+
+    [Required]
+    public double MinSalary { get; set; } = 0;
+
+    [Required]
+    public double MaxSalary { get; set; } = 0;
+    [Required]
+    public bool IsActive { get; set; } = true;
+
+    [Required]
+    public Guid JobCategoryId { get; set; }
+
+    [Required]
+    public Guid JobTypeId { get; set; }
+
+    [Required]
+    public DateTime Expiration { get; set; }
+
+    public string CreatedById { get; set; }
+
+    public UpdateJobPostRequest(string title, string description, Guid jobLocationId, double minSalary, double maxSalary, bool isActive, Guid jobCategoryId, Guid jobTypeId, DateTime expiration, string createdById)
+    {
+        Title = title;
+        Description = description;
+        JobLocationId = jobLocationId;
+        MinSalary = minSalary;
+        MaxSalary = maxSalary;
+        IsActive = isActive;
+        JobCategoryId = jobCategoryId;
+        JobTypeId = jobTypeId;
+        Expiration = expiration;
+        CreatedById = createdById;
+    }
+}

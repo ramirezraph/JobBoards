779d9dd6a518725bacb4e78c09db4a21530f3787
Raphael Ramirez <ramirez.raph@yahoo.com>
2023-04-03 13:55:18 +0800

    added most filters

--- JobBoards.WebApplication/ViewModels/Jobs/IndexViewModel.cs
+++ JobBoards.WebApplication/ViewModels/Jobs/IndexViewModel.cs
@@ -9,4 +9,15 @@ public class IndexViewModel
     public List<JobLocation> JobLocations { get; set; } = new();
     public List<JobType> JobTypes { get; set; } = new();
     public bool HasWriteAccess { get; set; } = false;
+    public FilterForm Filters { get; set; } = new FilterForm();
+
+    public class FilterForm
+    {
+        public Guid? JobCategoryId { get; set; }
+        public Guid? JobLocationId { get; set; }
+        public Guid? JobTypeId { get; set; }
+        public double? MinSalary { get; set; }
+        public double? MaxSalary { get; set; }
+    }
 }
+

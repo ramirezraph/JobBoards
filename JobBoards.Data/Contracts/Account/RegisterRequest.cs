064bc7cbee7cce93f740c758a1eadb239bfaff4d
Raphael Ramirez <ramirez.raph@yahoo.com>
2023-04-26 17:00:58 +0800

    password validation for api

--- JobBoards.Data/Contracts/Account/RegisterRequest.cs
+++ JobBoards.Data/Contracts/Account/RegisterRequest.cs
@@ -8,6 +8,8 @@ public class RegisterRequest
     public string FullName { get; set; } = default!;
     [Required]
     public string Email { get; set; } = default!;
+
     [Required]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Password must have a minimum of eight characters, at least one letter, one number and one special character")]
     public string Password { get; set; } = default!;
 }
